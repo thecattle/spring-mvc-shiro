4e29d48dadb14125a814d3797d83587750bab0b7
sunpeng <sunpeng1@souche.com>
2017-09-03 23:51:42 +0800

    shiro学习之集成 spring和登录认证

--- src/main/java/com/sunp/controller/LoginController.java
+++ src/main/java/com/sunp/controller/LoginController.java
@@ -2,6 +2,9 @@ package com.sunp.controller;
 
 import com.sunp.model.User;
 import com.sunp.service.UserService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,18 +30,54 @@ import java.util.Map;
 @RequestMapping(value = "/user")
 public class LoginController {
 
-    private Logger logger= LoggerFactory.getLogger(LoginController.class);
+    private Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     @Autowired
     private UserService userService;
 
 
-    @RequestMapping(value = "/getUserList",method = RequestMethod.GET)
+    //方便调试 直接用 get
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
     @ResponseBody
-    public Map<String,Object> getUserList(){
+    public Map<String, Object> login(String username, String password) {
+        Map<String, Object> map = new HashMap<>();
+        logger.info("开始登录");
+        //构建登录 token
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+        //设置记住我
+        token.setRememberMe(true);
+        //获取当前登录用户
+        Subject currentUser = SecurityUtils.getSubject();
+        try {
+            //登录
+            currentUser.login(token);
+            //判断用户状态是否已经被认证
+            if (currentUser.isAuthenticated()) {
+                map.put("msg", "登录成功");
+            } else {
+                map.put("msg", "系统异常，请重试");
+            }
+        } catch (UnknownAccountException uae) {
+            map.put("msg", "未知用户");
+        } catch (IncorrectCredentialsException ice) {
+            map.put("msg", "认证失败");
+        } catch (LockedAccountException lae) {
+            map.put("msg", "账户已锁定");
+        } catch (ExcessiveAttemptsException eae) {
+            map.put("msg", "登录失败次数过多");
+        } catch (AuthenticationException ae) {
+            map.put("msg", "ae.getMessage()");
+        }
+
+        return map;
+    }
+
+    @RequestMapping(value = "/getUserList", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, Object> getUserList() {
         logger.info("user/getUserList init");
-        Map<String,Object> map=new HashMap<>();
-        map.put("users",userService.getUserList());
+        Map<String, Object> map = new HashMap<>();
+        map.put("users", userService.getUserList());
         return map;
     }
 }
---
+++ src/main/java/com/sunp/shiro/MyRealm.java
@@ -0,0 +1,50 @@
+package com.sunp.shiro;
+
+import com.sunp.model.User;
+import com.sunp.service.UserService;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.crypto.SecureRandomNumberGenerator;
+import org.apache.shiro.crypto.hash.Sha256Hash;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class MyRealm extends AuthorizingRealm {
+    @Autowired
+    private UserService userService;
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    /**
+     * 认证用户的操作
+     * @param authenticationToken
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+
+        UsernamePasswordToken token = (UsernamePasswordToken)authenticationToken;
+        //从假数据源中拿当前用户数据
+        User userDao = userService.getUserInfo(token.getUsername());
+        if (userDao==null){
+            //扔出个异常，currentUser.login(token);可以 catch 到
+            throw new UnknownAccountException("未知用户");
+        }
+
+        //如果数据库中存在这个用户名
+        if(userDao.getUserName().equals(token.getUsername())){
+            //创建认证对象，传入数据库存在的对象账号和密码，内部会根据 currentUser.login(token) 的 token 进行比较
+            AuthenticationInfo authcInfo = new SimpleAuthenticationInfo(userDao.getUserName(), userDao.getUserPassword(), this.getName());
+            return authcInfo;
+        }
+        return null;
+    }
+
+}
